53bb0f7df0008619ed093ff9f1f497606f4d929e
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-14 17:29:50 -0500

    v.0.13.0 Se ajusta input nomina

--- templates/directivas/pr_nom_etapa_html.php
+++ templates/directivas/pr_nom_etapa_html.php
@@ -3,7 +3,9 @@ namespace html;
 
 use gamboamartin\errores\errores;
 use gamboamartin\proceso\controllers\controlador_pr_etapa;
+use gamboamartin\proceso\controllers\controlador_pr_nom_etapa;
 use gamboamartin\system\html_controler;
+use models\nom_nomina;
 use models\pr_etapa;
 use models\pr_nom_etapa;
 use PDO;
@@ -11,6 +13,136 @@ use stdClass;
 
 
 class pr_nom_etapa_html extends html_controler {
+
+    private function asigna_inputs(controlador_pr_nom_etapa $controler, stdClass $inputs): array|stdClass
+    {
+        $controler->inputs->select = new stdClass();
+        $controler->inputs->select->pr_etapa_id = $inputs->selects->pr_etapa_id;
+        $controler->inputs->select->nom_nomina_id = $inputs->selects->nom_nomina_id;
+        return $controler->inputs;
+    }
+
+    public function genera_inputs_alta(controlador_pr_nom_etapa $controler, PDO $link): array|stdClass
+    {
+        $inputs = $this->init_alta(link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar inputs',data:  $inputs);
+
+        }
+        $inputs_asignados = $this->asigna_inputs(controler:$controler, inputs: $inputs);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar inputs',data:  $inputs_asignados);
+        }
+
+        return $inputs_asignados;
+    }
+
+    private function genera_inputs_modifica(controlador_pr_nom_etapa $controler,PDO $link,
+                                            stdClass $params = new stdClass()): array|stdClass
+    {
+        $inputs = $this->init_modifica(link: $link, row_upd: $controler->row_upd, params: $params);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar inputs',data:  $inputs);
+
+        }
+        $inputs_asignados = $this->asigna_inputs(controler:$controler, inputs: $inputs);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar inputs',data:  $inputs_asignados);
+        }
+
+        return $inputs_asignados;
+    }
+
+    private function init_alta(PDO $link): array|stdClass
+    {
+        $keys_selects = array();
+        $selects = $this->selects_alta(keys_selects: $keys_selects, link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar selects',data:  $selects);
+        }
+
+        $texts = $this->texts_alta(row_upd: new stdClass(), value_vacio: true);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar texts',data:  $texts);
+        }
+
+        $alta_inputs = new stdClass();
+        $alta_inputs->selects = $selects;
+        $alta_inputs->texts = $texts;
+
+        return $alta_inputs;
+    }
+
+    private function init_modifica(PDO $link, stdClass $row_upd, stdClass $params = new stdClass()): array|stdClass
+    {
+        $selects = $this->selects_modifica(link: $link, row_upd: $row_upd);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar selects',data:  $selects);
+        }
+
+        $texts = $this->texts_alta(row_upd: $row_upd, value_vacio: false, params: $params);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar texts',data:  $texts);
+        }
+
+        $alta_inputs = new stdClass();
+        $alta_inputs->texts = $texts;
+        $alta_inputs->selects = $selects;
+        return $alta_inputs;
+    }
+
+    public function inputs_pr_etapa_proceso(controlador_pr_nom_etapa $controlador,
+                                            stdClass $params = new stdClass()): array|stdClass
+    {
+        $inputs = $this->genera_inputs_modifica(controler: $controlador,
+            link: $controlador->link, params: $params);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar inputs',data:  $inputs);
+        }
+        return $inputs;
+    }
+
+    protected function selects_alta(array $keys_selects, PDO $link): array|stdClass
+    {
+        $selects = new stdClass();
+
+        $select = (new pr_etapa_html(html:$this->html_base))->select_pr_etapa_id(
+            cols: 6, con_registros:true, id_selected:-1,link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select',data:  $select);
+        }
+        $selects->pr_etapa_id = $select;
+
+        $select = (new nom_nomina_html(html:$this->html_base))->select_nom_nomina_id(
+            cols: 6, con_registros:true, id_selected:-1,link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select',data:  $select);
+        }
+        $selects->nom_nomina_id = $select;
+
+        return $selects;
+    }
+
+    private function selects_modifica(PDO $link, stdClass $row_upd): array|stdClass
+    {
+        $selects = new stdClass();
+
+        $select = (new pr_etapa_html(html:$this->html_base))->select_pr_etapa_id(
+            cols: 6, con_registros:true, id_selected:$row_upd->pr_etapa_id,link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select',data:  $select);
+        }
+        $selects->pr_etapa_id = $select;
+
+        $select = (new nom_nomina_html(html:$this->html_base))->select_nom_nomina_id(
+            cols: 6, con_registros:true, id_selected:$row_upd->nom_nomina_id,link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select',data:  $select);
+        }
+        $selects->pr_tipo_proceso_id = $select;
+
+        return $selects;
+    }
 
     public function select_pr_nom_etapa_id(int $cols, bool $con_registros, int $id_selected, PDO $link,
                                            bool $disabled = false, bool $required = false): array|string
