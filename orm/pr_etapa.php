3f8e7ff57528bfc4111ae1353172431d364d1917
Kevin <kevin.acuna@ivitec.com.mx>
2022-11-04 11:04:27 -0500

    v0.17.2 Se integra NAMESPACE a pr_etapa

--- orm/pr_etapa.php
+++ orm/pr_etapa.php
@@ -12,5 +12,7 @@ class pr_etapa extends modelo{
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
             columnas: $columnas);
+
+        $this->NAMESPACE = __NAMESPACE__;
     }
 }
