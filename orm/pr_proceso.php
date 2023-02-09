d425143f6034aae15aca6d3fcb71428e5dde477f
Javier Flores <javier.flores@ivitec.com.mx>
2023-02-09 16:49:13 -0600

    v5.1.4 Se integra ut pr proceso

---
+++ orm/pr_proceso.php
@@ -0,0 +1,25 @@
+<?php
+namespace gamboamartin\proceso\models;
+use base\orm\_modelo_parent;
+use PDO;
+
+class pr_proceso extends _modelo_parent {
+
+    public function __construct(PDO $link){
+        $tabla = 'pr_proceso';
+        $columnas = array($tabla=>false);
+        $campos_obligatorios[] = 'descripcion';
+        $campos_obligatorios[] = 'descripcion_select';
+
+        $tipo_campos['codigos'] = 'cod_1_letras_mayusc';
+
+
+
+        parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
+            columnas: $columnas, tipo_campos: $tipo_campos);
+
+        $this->NAMESPACE = __NAMESPACE__;
+    }
+
+
+}
