a633c27249e3352d7732bb70ee647fe526687378
mgamboa <gamboa.martin@gmail.com>
2024-03-11 13:37:48 -0600

    v19.5.0 Se ajusta selector pr etapa proceso

--- templates/directivas/pr_etapa_proceso_html.php
+++ templates/directivas/pr_etapa_proceso_html.php
@@ -158,12 +158,13 @@ class pr_etapa_proceso_html extends html_controler {
         return $selects;
     }
 
-    public function select_pr_etapa_proceso_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
+    public function select_pr_etapa_proceso_id(int $cols, bool $con_registros, int $id_selected, PDO $link,
+                                               array $filtro = array(), string $label = 'Etapa proceso'): array|string
     {
         $modelo = new pr_etapa_proceso(link: $link);
 
-        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
-            modelo: $modelo, label: 'Etapa proceso',required: true);
+        $select = $this->select_catalogo(cols: $cols, con_registros: $con_registros, id_selected: $id_selected,
+            modelo: $modelo, filtro: $filtro, label: $label, required: true);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select', data: $select);
         }
